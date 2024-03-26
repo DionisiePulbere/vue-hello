e00c4c5347f624fa008ab394166c1b0f9524a384
DionisiePulbere <dionisiepulbere@gmail.com>
2024-03-26 16:50:58 +0100

    feat: add project

--- js/script.js
+++ js/script.js
@@ -6,5 +6,11 @@
 const { createApp } = Vue;
 
 createApp({
-    
+    data(){
+        return {
+            message: 'Benvenuto in vueJS',
+            image: 'https://cdn.sanity.io/images/8edntncj/production/7cb22a8a9885468d96662f6ece61ceb2bc95666d-500x300.png',
+            alt: 'Logo vueJS'
+        };
+    },
 }).mount('#app');
